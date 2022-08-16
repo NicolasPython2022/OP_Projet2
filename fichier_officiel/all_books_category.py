0166f14f538ed52c8f7642337ac6e3aec3df126b
Nicolas Bonnefoie <team.devparis@gmail.com>
2022-08-16 16:57:59 +0200

    Mise a jour convention pep8

--- fichier_officiel/all_books_category.py
+++ fichier_officiel/all_books_category.py
@@ -1,35 +1,46 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 # Definition de ma function
 def get_all_books_category(category):
     """Retourne toutes les pages d'une category
-    
-    Cette function recupere via l'url d'une page de category, toute les pages de celle-ci,
+
+    Cette function recupere via l'url d'une page de category,
+    toute les pages de celle-ci,
     qui par une boucle for prendra en selection l'attribut du lien,
     qui sera ensuite concatainer a une variable nomme ici chemin.
 
-    Dans un premier temps je definit l'indice de la category par une url que je viens avec
-    la methode requests et BeautifulSoup, parsemer la page de l'url donner.
-    Je recupere ensuite les balises rechercher via la methode find de BeautifulSoup.
+    Dans un premier temps je definit l'indice de la category,
+    par une url que je viens avec la methode requests et
+    BeautifulSoup parsemer la page de l'url donner.
+    Je recupere ensuite les balises rechercher via
+    la methode find de BeautifulSoup.
 
-    Secondement je boucle avec for la balise un element de toutes mes balises identifier.
-    J'assimile a une var l'element identifier que je credite de l'attribut de celui-ci en "str".
+    Secondement je boucle avec for un element
+    de toutes mes balises identifier.
+    J'assimile a une var l'element identifier que je credite,
+    de l'attribut de celui-ci en "str".
     La methode ".split" me permet de diviser ma chaine en une liste,
     ou chaque mot est un element de la liste.
-    Je passe en separateur de division de ma chaine "/" a la methode split,
-    puis j'indique ensuite la valeur de l'attribut identifier et definit.
-    Je conditionne pour finir, que "category en param de ma function,
+    Je passe en separateur de division de ma chaine "/",
+    a la methode split, puis j'indique ensuite la valeur de
+    l'attribut identifier et definit.
+    Je conditionne pour finir que "category en param de ma function,
     soit strictement egal a ma var "cat" pour definir mon indice,
     et je "break" pour sortir de ma boucle.
-    
+
     Troisiement, j'extrait l'url de ma page pricipale,
-    que je formate en lui indiquant entre accolades, la category et l'indice separer.
+    que je formate en lui indiquant entre accolades,
+    la category et l'indice separer.
 
     J'extrait ensuite le nombres de resultats.
-    Puis je conditionne la pagination au nombre max de livres ce trouvant sur une page url,
-    dans une liste qui sera accrementer par une boucle "for" via la methode ".append",
-    et via la methode ".replace" je viens remplacer la chaine selectionner par un lien,
+    Puis je conditionne la pagination au nombre max de livres
+    ce trouvant sur une page url,
+    dans une liste qui sera accrementer par une boucle "for"
+    via la methode ".append",
+    et via la methode ".replace"
+    je viens remplacer la chaine selectionner par un lien,
     qui ici est ma var chemin.
 
     Je termine en retournant la valeur de ma function.
@@ -41,25 +52,28 @@ def get_all_books_category(category):
     ul = home_soup.find('ul', class_='nav-list')
     all_a = ul.find_all('a')
 
-    # Boucle qui recupere tts les urls des category pour trouver l'indice de la category donner
+    '''
+    Boucle qui recupere tts les urls des category
+    pour trouver l'indice de la category donner
+    '''
     for a in all_a:
-        cat_ind = a['href'].split("/")[-2]    
-        cat = cat_ind.split("_")[0]             
-        if category==cat:
-            indice=cat_ind.split("_")[-1]    
-            break  
+        cat_ind = a['href'].split("/")[-2]
+        cat = cat_ind.split("_")[0]
+        if category == cat:
+            indice = cat_ind.split("_")[-1]
+            break
 
     # Extraction de l'url de la page principale
-    main_url=f'http://books.toscrape.com/catalogue/category/books/{category}_{indice}/index.html'
-    main_page=requests.get(main_url)
-    soup = BeautifulSoup(main_page.content,"html.parser")
+    main_url = f'http://books.toscrape.com/catalogue/category/books/{category}_{indice}/index.html'
+    main_page = requests.get(main_url)
+    soup = BeautifulSoup(main_page.content, "html.parser")
 
     # Extraction du nb de results
-    form_results= soup.find_all('strong')[1]
+    form_results = soup.find_all('strong')[1]
     number_results = int(form_results.string)
-    
+
     # Condition sur la pagination
-    url_books=[]
+    url_books = []
     if number_results <= 20:
         h3 = soup.select('h3 a')
         chemin = "http://books.toscrape.com/catalogue/"
@@ -67,9 +81,9 @@ def get_all_books_category(category):
             url_books.append(a['href'].replace('../../../', chemin))
     else:
         pagination = soup.find_all('li', class_='current')[0]
-        pages_number=str(pagination.string.strip()).split(' ')[-1]
+        pages_number = str(pagination.string.strip()).split(' ')[-1]
         pages_number = int(pages_number)
-        for num_page in range(1,pages_number+1):
+        for num_page in range(1, pages_number+1):
             page_url = f'http://books.toscrape.com/catalogue/category/books/{category}_{indice}/page-{num_page}.html'
             page = requests.get(page_url)
             page_soup = BeautifulSoup(page.content, "html.parser")
@@ -77,8 +91,8 @@ def get_all_books_category(category):
             chemin = "http://books.toscrape.com/catalogue/"
             for a in h3:
                 url_books.append(a['href'].replace('../../../', chemin))
-    
-    return url_books  
+
+    return url_books
 
 
 '''
--- fichier_officiel/main.py
+++ fichier_officiel/main.py
@@ -11,46 +11,55 @@ category_book = requests.get(url_home)
 home_soup = BeautifulSoup(category_book.content, "html.parser")
 ul = home_soup.find('ul', class_='nav-list')
 name_cat = ul.find_all("a")
-#print(name_cat)
+# print(name_cat)
 list_categories = []
 for a in name_cat:
     cat_ind = a.text.strip()
-    cat_ind=cat_ind.lower()
+    cat_ind = cat_ind.lower()
     if " " in cat_ind:
-        cat_ind = cat_ind.replace(" ","-")
-    #print(cat_ind)
+        cat_ind = cat_ind.replace(" ", "-")
+    # print(cat_ind)
     list_categories.append(cat_ind)
 
-list_categories.remove("books")   
-#print(list_categories)
+list_categories.remove(" books ")
+# print(list_categories)
 
 # Creation du header du fichier csv
-header = ["Title","Category","URL_image","Product_description","UCP","Price_excluding","Price_including","Availability","Number_reviews"]
+header = [
+    "Title",
+    "Category",
+    "URL_image",
+    "Product_description",
+    "UCP", "Price_excluding",
+    "Price_including",
+    "Availability",
+    "Number_reviews"]
+
 for categorie in list_categories:
-    #print(categorie)
-    url_books = get_all_books_category(categorie)  
+    # print(categorie)
+    url_books = get_all_books_category(categorie)
     # Creation du fichier csv avant la boucle
     print(f'Creation de {categorie}.csv ......')
-    csvfile=open(f'./category_csv/{categorie}.csv', 'w')
-    writer = csv.DictWriter(csvfile,delimiter='|', fieldnames=header)
+    csvfile = open(f'./category_csv/{categorie}.csv', 'w')
+    writer = csv.DictWriter(csvfile, delimiter='|', fieldnames=header)
     writer.writeheader()
 
     # Boucle qui recupere les infos de tout les livres par category
     for i in range(len(url_books)):
-        url_book=url_books[i]
+        url_book = url_books[i]
         book_data = get_infos_book(url_book)
-        #print(book_data)
-    # Creation de mon dictionnaire de donnees avec la valeur retourner de ma function "get_infos_book()"
+        # print(book_data)
+    # Creation de mon dict avec la valeur de ma function "get_infos_book()"
         dict_data = [
             {'Title': book_data['title'],
-            'Category': book_data['category'],
-            'URL_image': book_data["url_image"],
-            'Product_description': book_data['product_description'],
-            'UCP': book_data['ucp'],
-            'Price_excluding': book_data['price_excluding'],
-            'Price_including': book_data["price_including"],
-            'Availability': book_data['availability'],
-            'Number_reviews': book_data['number_reviews']}
+             'Category': book_data['category'],
+             'URL_image': book_data["url_image"],
+             'Product_description': book_data['product_description'],
+             'UCP': book_data['ucp'],
+             'Price_excluding': book_data['price_excluding'],
+             'Price_including': book_data["price_including"],
+             'Availability': book_data['availability'],
+             'Number_reviews': book_data['number_reviews']}
         ]
 
     # Rentrer les donnees dans le fichier csv
@@ -58,9 +67,5 @@ for categorie in list_categories:
             writer. writerow(data)
 
     # Fermer le fichier csv
-    csvfile.close() 
+    csvfile.close()
     print(f'Fin Creation de {categorie}.csv')
-
-
-
-
